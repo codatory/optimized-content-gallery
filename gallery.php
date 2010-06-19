94b6f5145f6a51b28942391639d1135f2559fcd5
Alex Conner <alex@alxconn.com>
2010-06-18 20:14:53 -0400

    Modified gallery to use article thumbnail from wordpress

--- gallery.php
+++ gallery.php
@@ -39,10 +39,19 @@
 					     <p><?php the_content_rss('', 0, '', $wordquantity); ?></p>
 					  <?php
 						}
+						/* Grab some variables for below */
+						$width = get_option('gallery-width');
+						$height = get_option('gallery-height');
 						?>
-					  <a href="<?php the_permalink() ?>" title="Read More" class="open"></a>
-					  <img src="<?php $key="articleimg"; echo get_post_meta($post->ID, $key, true); ?>" alt="<?php $key="alttext"; echo get_post_meta($post->ID, $key, true); ?>" class="full" />
-					  <img src="<?php $key=$imgthumb; echo get_post_meta($post->ID, $key, true); ?>" alt="<?php $key="alttext"; echo get_post_meta($post->ID, $key, true); ?>" class="thumbnail" />
+						<?php 
+							the_post_thumbnail(array($width,$height), array('class' => 'full')); 
+						?>
+						
+						<a href="<?php the_permalink() ?>" title="Read More" class="open"></a>
+						
+						<?php
+							the_post_thumbnail(array($width,$height), array('class' => 'thumbnail'));
+						?>
 				  </div>
 			 <?php 
 			 } ?>
